25328d7446322952a9fde44ad246c2194cc00c47
JrHuaancas <ci70047724@iestp4dejunio.edu.pe>
2023-07-04 11:07:21 -0500

    web

---
+++ mate.js
@@ -0,0 +1,36 @@
+//Comentario de una linea
+alert("Hola...");
+/*Comentario de varias lineas */ 
+
+var codigo; 
+let nombre;
+
+const dato="2023";
+var edad;
+edad=20;
+
+if (edad > 18){
+    alert("Mayor de edad");
+
+}
+
+else{
+    alert("noo");
+}
+
+var contador;
+for (contador=0; contador <=20; contador++) {
+    console.log(contador);
+}
+
+function f_suma(n1, n2){
+    var suma;
+    suma = n1 + n2;
+    return suma;
+}
+
+var resultado;
+resultado = f_suma(20,5);
+alert("La suma es:" + resultado);
+
+
---
+++ ope.js
@@ -0,0 +1,4 @@
+function respuesta(){
+    var numero = document.getElementById("numero").value;
+    document.getElementById("resultado").innerHTML = "<h1>" +numero+ "</h1>";
+}
